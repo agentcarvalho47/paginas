9dbe406f624fdd930c0d55e28379547fe7e89549
agentcarvalho47 <carvalhoandre568@gmail.com>
2019-11-08 09:39:21 -0300

    New PageView

--- paginas/urls.py
+++ paginas/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 
 from .views import HomePageView
+from .views import SobrePageView
 urlpatterns = [
-    path('', HomePageView.as_view(), name ='home')
+    path('', HomePageView.as_view(), name ='home'),
+    path('sobre', SobrePageView.as_view(), name = 'sobre')
 ]
