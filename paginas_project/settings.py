8e890e670d29bf2774a33cf3b6492c6fc090515e
agentcarvalho47 <carvalhoandre568@gmail.com>
2019-12-06 12:13:36 -0300

    Final.version

--- paginas_project/settings.py
+++ paginas_project/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'posts.apps.PostsConfig',
     'paginas.apps.PaginasConfig',
     'django.contrib.admin',
     'django.contrib.auth',
